4afe73506ee5000f056169f40ff8306c1f15a589
grayd <sharin@you-x.ru>
2024-09-12 09:33:42 +0300

    ошибка при загрузки данных

--- db_manager.py
+++ db_manager.py
@@ -2,7 +2,6 @@ import psycopg2
 from vacancy import Vacancy
 
 class DBManager:
-    """Менеджер для работы с базой данных PostgreSQL"""
     def __init__(self, host, database, user, password):
         self.connection = psycopg2.connect(
             host=host,
@@ -12,11 +11,43 @@ class DBManager:
         )
         self.cursor = self.connection.cursor()
 
+        # Проверка наличия таблицы vacancies и создание, если ее нет
+        self.cursor.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'vacancies')")
+        if not self.cursor.fetchone()[0]:
+            self.cursor.execute("""
+                CREATE TABLE vacancies (
+                    id SERIAL PRIMARY KEY,
+                    employer_name TEXT,
+                    name TEXT,
+                    salary NUMERIC,
+                    alternate_url TEXT
+                )
+            """)
+            self.connection.commit()
+
+        # Проверка наличия столбцов в таблице vacancies
+        self.cursor.execute("SELECT * FROM information_schema.columns WHERE table_name = 'vacancies'")
+        existing_columns = [column[3] for column in self.cursor.fetchall()]
+
+        if 'employer_name' not in existing_columns:
+            self.cursor.execute("ALTER TABLE vacancies ADD COLUMN employer_name TEXT")
+        if 'name' not in existing_columns:
+            self.cursor.execute("ALTER TABLE vacancies ADD COLUMN name TEXT")
+        if 'salary' not in existing_columns:
+            self.cursor.execute("ALTER TABLE vacancies ADD COLUMN salary NUMERIC")
+        if 'alternate_url' not in existing_columns:
+            self.cursor.execute("ALTER TABLE vacancies ADD COLUMN alternate_url TEXT")
+
+        self.connection.commit()
+
+
+
     def save_vacancies(self, vacancies):
         """Сохранение списка вакансий в базу данных"""
-        for vacancy in vacancies:
-            self.cursor.execute("""
-                INSERT INTO vacancies (employer_name, name, salary, alternate_url)
-                VALUES (%s, %s, %s, %s)
-            """, (vacancy.employer_name, vacancy.name, vacancy.salary, vacancy.alternate_url))
+        values = [(v.employer_name, v.name, v.salary, v.alternate_url) for v in vacancies]
+        self.cursor.executemany("""
+            INSERT INTO vacancies (employer_name, name, salary, alternate_url)
+            VALUES (%s, %s, %s, %s)
+        """, values)
         self.connection.commit()
+
