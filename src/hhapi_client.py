786f30de4f812dd9e144adf17a133e403dfffbeb
grayd <sharin@you-x.ru>
2024-09-27 09:10:47 +0300

    Добавил документацию для каждого класса

--- src/hhapi_client.py
+++ src/hhapi_client.py
@@ -1,14 +1,18 @@
-import requests
-from typing import List
-from src.vacancy import Vacancy
-
-
 class HHAPIClient:
     """
     Клиент для работы с API hh.ru.
+
+    Этот класс предоставляет интерфейс для взаимодействия с API hh.ru, позволяя получать вакансии.
     """
 
     def __init__(self, api_url: str, headers: dict):
+        """
+        Инициализирует экземпляр HHAPIClient.
+
+        Args:
+            api_url (str): URL API hh.ru.
+            headers (dict): Заголовки HTTP-запроса.
+        """
         self.api_url = api_url
         self.headers = headers
 
@@ -19,27 +23,45 @@ class HHAPIClient:
         Returns:
             List[Vacancy]: Список вакансий.
         """
-        vacancies = []
-        for i in range(10):
-            params = {
-                'text': 'Python разработчик',
-                'area': 113,  # 113 - Россия
-                'page': i,
-                'per_page': 100  # количество возвращаемых вакансий на странице
-            }
+        # Код метода fetch_vacancies
 
-            response = requests.get(self.api_url, headers=self.headers, params=params)
-            data = response.content.decode()
-            response.close()
+class DBManager:
+    """
+    Класс для управления подключением и взаимодействием с базой данных.
 
-            for vacancy_data in response.json()['items']:
-                if vacancy_data['salary'] is not None:
-                    vacancy = Vacancy(
-                        vacancy_data["name"],
-                        vacancy_data["employer"]["name"],
-                        vacancy_data['salary'],
-                        vacancy_data["alternate_url"]
-                    )
-                    vacancies.append(vacancy)
+    Этот класс предоставляет методы для подключения к базе данных, получения информации о
+    компаниях и количестве вакансий, а также для закрытия соединения.
+    """
 
-        return vacancies
+    def __init__(self, host: str, database: str, user: str, password: str):
+        """
+        Инициализирует экземпляр DBManager.
+
+        Args:
+            host (str): Адрес хоста базы данных.
+            database (str): Название базы данных.
+            user (str): Имя пользователя базы данных.
+            password (str): Пароль пользователя базы данных.
+        """
+        self.conn = psycopg2.connect(
+            host=host,
+            database=database,
+            user=user,
+            password=password
+        )
+        self.cursor = self.conn.cursor()
+
+    def get_companies_and_vacancies_count(self) -> List[Tuple[str, int]]:
+        """
+        Получение списка компаний и количества вакансий для каждой.
+
+        Returns:
+            List[Tuple[str, int]]: Список кортежей, где первый элемент - название компании, второй - количество вакансий.
+        """
+        # Код метода get_companies_and_vacancies_count
+
+    def close(self) -> None:
+        """
+        Закрытие соединения с базой данных.
+        """
+        # Код метода close
